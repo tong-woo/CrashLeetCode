c20d671dfdb8feae6a15ed9d24c4d3a7aa35da7f
Tong Wu <tong.wu4@student.uva.nl>
2023-03-25 10:17:25 +0100

    Add Missing Integer problem

---
+++ src/main/java/com/tong/leetcode/problem/hash/MissingInteger.java
@@ -0,0 +1,23 @@
+package com.tong.leetcode.problem.hash;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MissingInteger {
+    public int solution(int[] A) {
+        // Implement your solution here
+        int res = 1;
+        boolean found = false;
+        Set<Integer> hashSet = new HashSet<>();
+        for (int i : A) {
+            if (i > 0) hashSet.add(i);
+        }
+        for (int i = 1; i <= A.length + 1 && !found; i++) {
+            if (!hashSet.contains(i)) {
+                res = i;
+                found = true;
+            }
+        }
+        return res;
+    }
+}
