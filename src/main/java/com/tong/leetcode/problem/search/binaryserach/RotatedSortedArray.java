b3fabdc1dac265806f375149b5cce7340a1c9d58
Tong Wu <tong.wu4@student.uva.nl>
2023-04-03 12:54:09 +0200

    Add new binary search problem

---
+++ src/main/java/com/tong/leetcode/problem/search/binaryserach/RotatedSortedArray.java
@@ -0,0 +1,21 @@
+package com.tong.leetcode.problem.search.binaryserach;
+
+public class RotatedSortedArray {
+
+  public int findMin(int[] nums) {
+    int maxRotateTime = nums.length;
+    int left = 1;
+    int right = maxRotateTime - 1;
+    while (left < right) {
+      int mid = left + (right - left) / 2;
+      if (nums[mid] > nums[right]) {
+        // like nums = [4,5,6,7,0,1,2],
+        // rotate time is not big enough
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
+    }
+    return nums[right];
+  }
+}
